8bd0ec2ad6e59c6ebe72b3247f1186655154a84c
veerababu-chotu <92910665+veerababu-chotu@users.noreply.github.com>
2021-10-23 12:45:11 +0530

    Add files via upload

---
+++ program.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	printf("student\tage\tcgpa\n");
+	printf("chotu\t19\t8.2\n");
+	printf("hari\t21\t8.7\n");
+	printf("rp\t21\t8.8\n");
+	printf("kishore\t20\t8.5\n");
+}   
