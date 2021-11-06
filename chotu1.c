4f442103ea368c1a101e7595c6da768ee3432c22
veerababu-chotu <92910665+veerababu-chotu@users.noreply.github.com>
2021-11-06 00:01:17 -0700

    Add files via upload

---
+++ chotu1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,s,q,x,d,c;
+	a=30;
+	b=20;
+	
+	s=a+b;
+	
+	
+	printf("add of %d and %d is %d\n",a,b,s);
+	q=a-b;
+	printf("sub of %d and %d is %d\n",a,b,q);
+	x=a*b;
+	printf("mul of %d and %d is %d\n",a,b,x);
+	d=a/b;
+	printf("div of %d and %d is %d\n",a,b,d);
+	c=a%b;
+	printf("reminder of %d and %d is %d\n",a,b,c);
+      
+}   
+
